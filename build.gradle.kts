6c6c336ff20da193a9230d11f7ddb6f6b39c7e95
xxxwarrior <sklmasha@yandex.ru>
2021-02-02 12:56:19 +0300

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
 plugins {
     application
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
     id("com.justai.jaicf.jaicp-build-plugin") version "0.1.1"
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "com.justai.jaicf"
 version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 val jaicf = "0.10.1"
 val logback = "1.2.3"
@@ -30,6 +37,14 @@ dependencies {
     implementation("com.justai.jaicf:core:$jaicf")
     implementation("com.justai.jaicf:jaicp:$jaicf")
     implementation("com.justai.jaicf:caila:$jaicf")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0-RC1")
+    implementation("com.beust:klaxon:5.4")
+
 }
 
 tasks {
@@ -47,4 +62,16 @@ tasks.create("stage") {
 
 tasks.withType<com.justai.jaicf.plugins.jaicp.build.JaicpBuild> {
     mainClassName.set(application.mainClassName)
+}
+
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
