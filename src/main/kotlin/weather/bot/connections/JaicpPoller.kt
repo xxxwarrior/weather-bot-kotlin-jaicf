a45aa31da2082a6b2523a1b478e8356bb75523ec
xxxwarrior <sklmasha@yandex.ru>
2021-02-02 12:59:16 +0300

    "Rename"

--- src/main/kotlin/weather/bot/connections/JaicpPoller.kt
+++ src/main/kotlin/weather/bot/connections/JaicpPoller.kt
@@ -1,11 +1,11 @@
-package com.justai.jaicf.template.connections
+package weather.bot.connections
 
 import com.justai.jaicf.channel.jaicp.JaicpPollingConnector
 import com.justai.jaicf.channel.jaicp.channels.ChatApiChannel
 import com.justai.jaicf.channel.jaicp.channels.ChatWidgetChannel
 import com.justai.jaicf.channel.jaicp.channels.TelephonyChannel
-import com.justai.jaicf.template.accessToken
-import com.justai.jaicf.template.templateBot
+import weather.bot.accessToken
+import weather.bot.templateBot
 
 fun main() {
     JaicpPollingConnector(
