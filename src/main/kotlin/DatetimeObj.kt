9e77b2115f36f327bcb6d9d2f43b14fd469f1956
xxxwarrior <sklmasha@yandex.ru>
2021-02-02 12:58:17 +0300

    "Datetime data class"

---
+++ src/main/kotlin/DatetimeObj.kt
@@ -0,0 +1,28 @@
+import com.beust.klaxon.*
+
+private val klaxon = Klaxon()
+
+data class DatetimeObj (
+    val year: String,
+    val month: String,
+    val day: String,
+    val dayOfWeek: String,
+    val timestamp: Long,
+
+    @Json(name = "exact_values")
+    val exactValues: ExactValues,
+
+    val value: String
+) {
+    fun toJson() = klaxon.toJsonString(this)
+
+    companion object {
+        fun fromJson(json: String) = klaxon.parse<DatetimeObj>(json)
+    }
+}
+
+data class ExactValues (
+    val year: Long,
+    val month: Long,
+    val day: Long
+)
