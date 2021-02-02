919d3a1a365d16162d83d023cd8b49766c9ccbd8
xxxwarrior <sklmasha@yandex.ru>
2021-02-02 14:19:04 +0300

    "Openweathermap api"

---
+++ src/main/kotlin/WeatherApi.kt
@@ -0,0 +1,17 @@
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+
+var apiKey = "8f6265a81af3154d852b30ebf630da10"
+
+class WeatherApi {
+
+    fun getWeather(city: String, timestamp: String? = null): WeatherObj? {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://api.openweathermap.org/data/2.5/${if (timestamp != null) "forecast" else "weather"}?q=$city&appid=$apiKey&lang=ru&units=metric")
+            .build()
+        val response = client.newCall(request).execute()
+        return response.body?.string()?.let { WeatherObj.fromJson(it) }
+    }
+}
