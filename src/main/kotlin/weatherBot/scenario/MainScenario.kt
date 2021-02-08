83215e1b90d60791cbcb441674ddc3c3b2eb2194
xxxwarrior <sklmasha@yandex.ru>
2021-02-08 14:30:21 +0300

    Correct some phrases

--- src/main/kotlin/weatherBot/scenario/MainScenario.kt
+++ src/main/kotlin/weatherBot/scenario/MainScenario.kt
@@ -6,7 +6,6 @@ import com.beust.klaxon.KlaxonException
 import com.justai.jaicf.activator.caila.caila
 import com.justai.jaicf.model.scenario.Scenario
 
-
 object MainScenario : Scenario() {
 
     init {
@@ -17,8 +16,8 @@ object MainScenario : Scenario() {
             action {
                 reactions.image("https://sun9-22.userapi.com/sun9-40/impg/lyJ2HA2Pknf2b6zN_Q4_bnfAPAR-Dq-Uan5qXA/hTv3Nt0BLoc.jpg?size=512x337&quality=96&proxy=1&sign=57c11956eaff2289d41a3eff9de758f9&type=album")
                 reactions.say(
-                    "Привет, я бот, который расскажет тебе о погоде, просто скажи мне кодовую фразу 'Погода *твой город*'. \n" +
-                            "Можешь указать 'сегодня', 'завтра' или любую дату в ближайшие 5 дней"
+                    "Привет, я бот, который расскажет тебе о погоде. Напиши 'Погода *твой город*' или просто название города, " +
+                            "а я дам тебе прогноз на сегодня. \nМожешь также указать 'завтра', 'послезавтра' или любую дату на ближайшие 5 дней."
                 )
             }
         }
@@ -59,14 +58,13 @@ object MainScenario : Scenario() {
             action {
                 activator.caila?.run {
                     val city: String? = if (slots["city"] in arrayOf(
-                            "СПБ", "Спб", "спб", "Питер", "санкт петербург"
-                        )
+                            "СПБ", "Спб", "спб", "Петербург", "санкт петербург")
                     ) "Санкт-Петербург" else slots["city"]
+
                     if (city != null) {
                         try {
-                            val weather = slots["city"]?.let {
-                                WeatherApi().getWeather(city = it, timestamp = slots["date"])
-                            }
+                            val weather = WeatherApi().getWeather(city = city, timestamp = slots["date"])
+
                             if (slots["date"] == null) {
                                 reactions.say(
                                     "В городе ${city.capitalize()} сейчас ${weather?.main?.temp} градусов, " +
@@ -77,27 +75,27 @@ object MainScenario : Scenario() {
                             } else if (slots["date"] != null) {
                                 val timestamp = slots["date"]?.let { DatetimeObj.fromJson(it) }?.timestamp
                                 val timestampCut = timestamp.toString().take(10)
-                                var flag = false
+                                var isDateValid = false
                                 for (i in weather?.list!!) {
                                     if (timestampCut == i.dt.toString()) {
                                         reactions.say(
-                                            "Прогноз погоды в городе $city на ${i.dtTxt.take(10)}: температура ${i.main.temp} градусов, " +
-                                                    "${i.weather[0].description}. Ощущается как ${i.main.feelsLike}. Давление ${i.main.pressure} " +
-                                                    "мм рт. ст., влажность ${i.main.humidity}%. Скорость ветра ${i.wind.speed} м/c."
+                                            "Прогноз погоды в городе ${city.capitalize()} на ${i.dtTxt.take(10)}: температура ${i.main.temp} градусов, " +
+                                                    "будет ${i.weather[0].description}. Ожидается давление ${i.main.pressure} " +
+                                                    "мм рт. ст., влажность ${i.main.humidity}% при скорости ветра ${i.wind.speed} м/c."
                                         )
-                                        flag = true
+                                        isDateValid = true
                                         break
                                     }
                                 }
-                                if (!flag) {
-                                    reactions.say("У меня нет прогноза на эту дату, попробуй спросить прогноз на ближайшие пару дней.")
+                                if (!isDateValid) {
+                                    reactions.say("У меня нет прогноза на эту дату, я знаю прогноз только на ближайшие пять дней.")
                                 }
-                            } else {
-                                reactions.say("Что-то пошло не так, попробуй написать, например, 'Погода Москва'.")
                             }
                         } catch (e: KlaxonException) {
                             reactions.say("Я не самый умный бот. Попробуй написать просто 'Погода Рим'.")
                         }
+                    } else {
+                        reactions.say("С названием города возникли проблемы, может быть такого города нет? Попробуй, например, 'Погода Пекин'.")
                     }
                 }
             }
@@ -108,7 +106,12 @@ object MainScenario : Scenario() {
                 }
 
                 action {
-                    reactions.say("Напиши 'Погода *название города*', а я скажу тебе прогноз на сегодня.")
+                    reactions.sayRandom(
+                        "Просто назови город, а я дам тебе прогноз на сегодня.",
+                        "Я всего лишь бот и мало что понимаю. Ты уже пробовал(а) написать название города?",
+                        "Поробуй 'Погода *название города*'",
+                        "А это точно город?"
+                    )
                 }
             }
         }
