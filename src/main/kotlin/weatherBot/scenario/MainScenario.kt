819890a8bef4a72408d8f39f56e46f1d895d5785
xxxwarrior <sklmasha@yandex.ru>
2021-02-19 23:33:14 +0300

    Structure refactoring

--- src/main/kotlin/weatherBot/scenario/MainScenario.kt
+++ src/main/kotlin/weatherBot/scenario/MainScenario.kt
@@ -1,6 +1,7 @@
 package weatherBot.scenario
 
-import DatetimeObj
+import DatetimeData
+import WeatherData
 import WeatherApi
 import com.beust.klaxon.KlaxonException
 import com.justai.jaicf.activator.caila.caila
@@ -20,7 +21,7 @@ object MainScenario : Scenario() {
                 reactions.image("https://sun9-22.userapi.com/sun9-40/impg/lyJ2HA2Pknf2b6zN_Q4_bnfAPAR-Dq-Uan5qXA/hTv3Nt0BLoc.jpg?size=512x337&quality=96&proxy=1&sign=57c11956eaff2289d41a3eff9de758f9&type=album")
                 reactions.say(
                     "Привет, я бот, который расскажет тебе о погоде. Напиши 'Погода *твой город*' или просто название города, " +
-                            "а я дам тебе прогноз на сегодня. \nМожешь также указать 'завтра', 'послезавтра' или любую дату на ближайшие 5 дней."
+                    "а я дам тебе прогноз на сегодня. \nМожешь также указать 'завтра', 'послезавтра' или любую дату на ближайшие 5 дней."
                 )
             }
         }
@@ -64,62 +65,59 @@ object MainScenario : Scenario() {
                             "СПБ", "Спб", "спб", "Петербург", "санкт петербург")
                     ) "Санкт-Петербург" else slots["city"]
 
-                    if (city != null) {
-                        try {
-                            val currentDate = LocalDate.now().toString().take(10)
-                            val date = slots["date"]?.let { DatetimeObj.fromJson(it) }?.value?.take(10)
-
-                            val weather = WeatherApi().getWeather(city = city, date = slots["date"] != null && currentDate != date)
-
-                            if (slots["date"] == null || currentDate == date) {
-                                reactions.say(
-                                    "В городе ${city.capitalize()} сейчас ${weather?.main?.temp} градусов, " +
-                                            "${weather?.weather?.get(0)?.description}. Ощущается как ${weather?.main?.feelsLike}. " +
-                                            "Давление ${weather?.main?.pressure} мм рт. ст., влажность ${weather?.main?.humidity}%. " +
-                                            "Скорость ветра ${weather?.wind?.speed} м/c."
-                                )
-                            } else {
-                                var isDateValid = false
-                                for (i in weather?.list!!) {
-                                    if (date == i.dtTxt.take(10) ) {
-                                        val parsedDate = LocalDate.parse(i.dtTxt.take(10))
-                                        val formatter = DateTimeFormatter.ofPattern("dd MMMM").withLocale(Locale("ru"))
-                                        val formattedDate = formatter.format(parsedDate)
-                                        reactions.say(
-                                            "Прогноз погоды в городе ${city.capitalize()} на $formattedDate: температура ${i.main.temp} градусов, " +
-                                                    "будет ${i.weather[0].description}. Ожидается давление ${i.main.pressure} " +
-                                                    "мм рт. ст., влажность ${i.main.humidity}% при скорости ветра ${i.wind.speed} м/c."
-                                        )
-                                        isDateValid = true
-                                        break
-                                    }
-                                }
-                                if (!isDateValid) {
-                                    reactions.say("У меня нет прогноза на эту дату, я знаю прогноз только на ближайшие пять дней.")
-                                }
-                            }
-                        } catch (e: KlaxonException) {
-                            reactions.say("Я не самый умный бот. Попробуй написать просто 'Погода Рим'.")
-                        }
-                    } else {
+                    if (city == null) {
                         reactions.say("С названием города возникли проблемы, может быть такого города нет? Попробуй, например, 'Погода Пекин'.")
+                    } else {
+                        val dateLength = 10
+
+                        val currentDate = LocalDate.now().toString().take(dateLength)
+                        val date = slots["date"]?.let { DatetimeData.fromJson(it) }?.value?.take(dateLength)
+
+                        // If there's a date and it's not today then it's a forecast
+                        val forecastNeeded = slots["date"] != null && currentDate != date
+
+                        val weather: WeatherData? = try { WeatherApi.getWeather(city, forecastNeeded) } catch (e: KlaxonException) { null }
+                        if (weather == null) {
+                            reactions.say("Я не самый умный бот. Попробуй написать просто 'Погода Рим'.")
+                        } else {
+                            if (forecastNeeded) {
+                                val dayWeather = weather.list?.find { date == it.dtTxt.take(dateLength) }
+                                if (dayWeather == null) {
+                                    reactions.say("У меня нет прогноза на эту дату, я знаю прогноз только на ближайшие пять дней.")
+                                } else {
+                                    val parsedDate = LocalDate.parse(date)
+                                    val formatter = DateTimeFormatter.ofPattern("dd MMMM").withLocale(Locale("ru"))
+                                    val formattedDate = formatter.format(parsedDate)
+                                    reactions.say(
+                                        "Прогноз погоды в городе ${city.capitalize()} на $formattedDate: температура ${dayWeather.main.temp} градусов, " +
+                                        "будет ${dayWeather.weather[0].description}. Ожидается давление ${dayWeather.main.pressure} " +
+                                        "мм рт. ст., влажность ${dayWeather.main.humidity}% при скорости ветра ${dayWeather.wind.speed} м/c.")
+                                }
+                            } else {
+                                reactions.say(
+                                    "В городе ${city.capitalize()} сейчас ${weather.main?.temp} градусов, " +
+                                    "${weather.weather?.get(0)?.description}. Ощущается как ${weather.main?.feelsLike}. " +
+                                    "Давление ${weather.main?.pressure} мм рт. ст., влажность ${weather.main?.humidity}%. " +
+                                    "Скорость ветра ${weather.wind?.speed} м/c.")
+                            }
+                        }
                     }
                 }
             }
+        }
 
-            state("fallback") {
-                activators {
-                    catchAll()
-                }
+        state("fallback") {
+            activators {
+                catchAll()
+            }
 
-                action {
-                    reactions.sayRandom(
-                        "Просто назови город, а я дам тебе прогноз на сегодня.",
-                        "Я всего лишь бот и мало что понимаю. Ты уже пробовал(а) написать название города?",
-                        "Поробуй 'Погода *название города*'",
-                        "А это точно город?"
-                    )
-                }
+            action {
+                reactions.sayRandom(
+                    "Просто назови город, а я дам тебе прогноз на сегодня.",
+                    "Я всего лишь бот и мало что понимаю. Ты уже пробовал(а) написать название города?",
+                    "Поробуй 'Погода *название города*'",
+                    "А это точно город?"
+                )
             }
         }
     }
