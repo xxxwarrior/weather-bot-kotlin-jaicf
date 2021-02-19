7a61d2dcd93648a32edbb7b7a4e790ec5e534275
xxxwarrior <sklmasha@yandex.ru>
2021-02-19 23:32:02 +0300

    getWeather func as a companion object

--- src/main/kotlin/WeatherApi.kt
+++ src/main/kotlin/WeatherApi.kt
@@ -4,12 +4,15 @@ import okhttp3.Request
 var apiKey = "8f6265a81af3154d852b30ebf630da10"
 
 class WeatherApi {
-    fun getWeather(city: String, date: Boolean = false): WeatherObj? {
-        val client = OkHttpClient()
-        val request = Request.Builder()
-            .url("http://api.openweathermap.org/data/2.5/${if (date) "forecast" else "weather"}?q=$city&appid=$apiKey&lang=ru&units=metric")
-            .build()
-        val response = client.newCall(request).execute()
-        return response.body?.string()?.let { WeatherObj.fromJson(it) }
+    companion object {
+        fun getWeather(city: String, forecastNeeded: Boolean = false): WeatherData {
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url("http://api.openweathermap.org/data/2.5/${if (forecastNeeded) "forecast" else "weather"}?q=$city&appid=$apiKey&lang=ru&units=metric")
+                .build()
+            val response = client.newCall(request).execute()
+            return response.body?.let { WeatherData.fromJson(it.string()) }!!
+        }
     }
+
 }
